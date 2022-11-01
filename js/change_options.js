bfb19ee388f100532c922f0fb482806c31d00bf9
iraida <ira.serq.1996@gmail.com>
2022-11-01 15:58:05 +0300

    Add select to mobile version

--- js/change_options.js
+++ js/change_options.js
@@ -1,36 +1,41 @@
-let buttons = document.querySelectorAll(".title-tab");
+if (screen.width <= "465"){
+    let select = document.querySelector(".select");
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
+    select.addEventListener("change", function () {
+        console.log(this.value);
+        changeOption(this.value);
     });
-
-    addEvent(buttons[i]);
 }
 
-function addEvent(button) {
-    button.addEventListener("click", function () {
-        let type = button.getAttribute("value");
+else {
+    let buttons = document.querySelectorAll(".title-tab");
 
-        switch (type) {
-            default:
-                changePrices(["1 200", "1 300", "1 400", "1 600"]);
-                changeFullCosts(["14 400", "10 400", "5 600"]);
-                break;
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
 
-            case "4":
-                changePrices(["1 500", "1 625", "1 750", "2 000"]);
-                changeFullCosts(["18 000", "13 000", "7 000"]);
-                break;
+            changeOption(buttons[i].value);
+        });
+    }
+}
 
-            case "5":
-                changePrices(["1 725", "1 868", "2 012", "2 300"]);
-                changeFullCosts(["20 700", "14 950", "8 050"]);
-                break;
-        }
-    });
+function changeOption(value) {
+    switch (value) {
+        default:
+            changePrices(["1 200", "1 300", "1 400", "1 600"]);
+            changeFullCosts(["14 400", "10 400", "5 600"]);
+            break;
+        case "4":
+            changePrices(["1 500", "1 625", "1 750", "2 000"]);
+            changeFullCosts(["18 000", "13 000", "7 000"]);
+            break;
+        case "5":
+            changePrices(["1 725", "1 868", "2 012", "2 300"]);
+            changeFullCosts(["20 700", "14 950", "8 050"]);
+            break;
+    }
 }
 
 function changePrices(array) {
